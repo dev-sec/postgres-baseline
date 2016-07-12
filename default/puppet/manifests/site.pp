8a69984a21a7c7e05aa3a0b86d4cec2b2686554c
Patrick Münch <patrick.muench1111@gmail.com>
2016-07-12 15:10:19 +0200

    update travis file
    Signed-off-by: Patrick Münch <patrick.muench1111@gmail.com>

--- default/puppet/manifests/site.pp
+++
@@ -1,9 +0,0 @@
-# Configure Postgresql Server as you normally would:
-
-class { '::postgresql::server':
-  postgres_password          => 'iloverandompasswordsbutthiswilldo',
-}
-
-class { '::postgres_hardening':
-  provider => 'puppetlabs/postgresql'
-}
