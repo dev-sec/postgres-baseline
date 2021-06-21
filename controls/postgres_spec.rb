a799e6cd413a9b754d686fc9397a38c9873c6fc6
Cookstyle <cookbooks@chef.io>
2021-06-21 13:47:34 -0700

    Cookstyle Bot Auto Corrections with Cookstyle 7.14.0 This change is automatically generated by the Cookstyle Bot using the latest version of Cookstyle (7.14.0). Adopting changes suggested by Cookstyle improves cookbook readability, avoids common coding mistakes, and eases upgrades to newer versions of the Chef Infra Client.
    ### Layout/EmptyLinesAroundBlockBody
      - **/Rakefile:49**: Extra empty line detected at block body end. (https://rubystyle.guide#empty-lines-around-bodies)
    
    Signed-off-by: Cookstyle <cookbooks@chef.io>

--- controls/postgres_spec.rb
+++ controls/postgres_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-# Copyright 2016, Patrick Muench
-# Copyright 2016-2019 DevSec Hardening Framework Team
+# Copyright:: 2016, Patrick Muench
+# Copyright:: 2016-2019 DevSec Hardening Framework Team
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
